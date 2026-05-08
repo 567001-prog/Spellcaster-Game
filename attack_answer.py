95f734b93d1f7252814d81dbe58ea1242fceef04
Lauren-Jung <567383@my.cuhsd.org>
2026-05-08 13:28:28 -0700

    Update attack_answer.py

--- attack_answer.py
+++ attack_answer.py
@@ -1,8 +1,7 @@
-
 import turtle as trtl
 
 wn = trtl.Screen()
-wn.setup(width=1.0, height=1.0) 
+wn.setup(width=1.0, height=1.0)
 wn.bgpic("output-onlinepngtools (2).gif")
 
 pen = trtl.Turtle()
@@ -10,29 +9,42 @@ pen.hideturtle()
 pen.penup()
 pen.color("white")
 
+
+# clears the text
+def clear_text():
+    pen.clear()
+
+
 def start():
     name = wn.textinput("Name Entry", "What is your name?")
-    
+
     if name is None:
         name = "loser"
 
-    pen.goto(0, 100)
-    
     intro_text = [
-        f"Hello, {name}! Your Kingdom is under attack.",
-        "You must defeat the monsters to save the kingdom. ",
-        "To fight the monster you must unscramble the spell and type",
-        "it into the text box before the time runs out.",
+        f"Hello, {name}! Your kingdom is under attack.",
+        "You must defeat the monsters to save the kingdom.",
+        "Unscramble the spell before time runs out.",
         "There are 4 monsters headed your way.",
+        "Press 'c' to continue.",
         "Good luck!"
     ]
 
-    current_y = 100
+    current_y = 120
+
     for line in intro_text:
         pen.goto(0, current_y)
-        pen.write(line, align="center", font=("Times New Roman", 25, "bold"))
-        current_y -= 30
+
+        pen.write(
+            line,
+            align="center",
+            font=("Times New Roman", 25, "bold")
+        )
+
+        current_y -= 50
+
 start()
+wn.listen()
+wn.onkey(clear_text, "c")
 
 wn.mainloop()
-
